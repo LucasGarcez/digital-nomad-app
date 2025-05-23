8347ca31f76054bc2919abfe2b9a4ba83049b03d
Lucas Garcez <lucasgarcezdev@gmail.com>
2025-05-24 09:14:22 +1000

    4.5 - Accordion animations

--- app/(protected)/city-details/components/CityDetailsTouristAttractions.tsx
+++ app/(protected)/city-details/components/CityDetailsTouristAttractions.tsx
@@ -1,19 +1,7 @@
 import { Accordion } from "@/src/components/Accordion";
-import { Box, BoxProps } from "@/src/components/Box";
-import { Icon } from "@/src/components/Icon";
+import { Box } from "@/src/components/Box";
 import { Text } from "@/src/components/Text";
-import { useAppTheme } from "@/src/theme/useAppTheme";
 import { City } from "@/src/types";
-import { Pressable, View } from "react-native";
-import Animated, {
-  interpolate,
-  interpolateColor,
-  SharedValue,
-  useAnimatedStyle,
-  useDerivedValue,
-  useSharedValue,
-  withTiming,
-} from "react-native-reanimated";
 
 export function CityDetailsTouristAttractions({
   touristAttractions,
@@ -27,7 +15,7 @@ export function CityDetailsTouristAttractions({
       <Box gap="s16">
         {touristAttractions.map((attraction) => (
           <Box key={attraction.id}>
-            <AccordionItem
+            <Accordion
               title={attraction.name}
               description={attraction.description}
             />
@@ -37,118 +25,3 @@ export function CityDetailsTouristAttractions({
     </Box>
   );
 }
-
-function AccordionItem({
-  title,
-  description,
-}: {
-  title: string;
-  description: string;
-}) {
-  const { colors, borderRadii } = useAppTheme();
-  const isExpanded = useSharedValue(false);
-  const onPress = () => {
-    isExpanded.value = !isExpanded.value;
-    progress.value = withTiming(isExpanded.value ? 0 : 1, { duration: 500 });
-  };
-
-  const progress = useSharedValue(0);
-
-  const animatedStyle = useAnimatedStyle(() => {
-    return {
-      backgroundColor: interpolateColor(
-        progress.value,
-        [0, 1],
-        [colors.transparent, colors.gray1]
-      ),
-      borderWidth: 2,
-      borderBottomWidth: interpolate(progress.value, [0, 1], [2, 0]),
-      borderColor: colors.gray1,
-      borderTopLeftRadius: borderRadii.default,
-      borderTopRightRadius: borderRadii.default,
-      borderBottomLeftRadius: interpolate(
-        progress.value,
-        [0, 1],
-        [borderRadii.default, 0]
-      ),
-      borderBottomRightRadius: interpolate(
-        progress.value,
-        [0, 1],
-        [borderRadii.default, 0]
-      ),
-    };
-  });
-
-  const iconStyle = useAnimatedStyle(() => ({
-    transform: [
-      {
-        rotate: isExpanded.value
-          ? withTiming("-180deg", { duration: 500 })
-          : withTiming("0deg", { duration: 500 }),
-      },
-    ],
-  }));
-
-  return (
-    <Pressable onPress={onPress}>
-      <Animated.View style={[animatedStyle]}>
-        <Box {...headerStyle}>
-          <Text variant="title16">{title}</Text>
-
-          <Animated.View style={iconStyle}>
-            <Icon name="Chevron-down" color={"gray2"} />
-          </Animated.View>
-        </Box>
-      </Animated.View>
-      <AccordionWrapper isExpanded={isExpanded}>
-        <Accordion
-          title={title}
-          description={description}
-          progress={progress}
-        />
-      </AccordionWrapper>
-    </Pressable>
-  );
-}
-
-function AccordionWrapper({
-  children,
-  isExpanded,
-}: {
-  children: React.ReactNode;
-  isExpanded: SharedValue<boolean>;
-}) {
-  const height = useSharedValue(0);
-
-  const derivedHeight = useDerivedValue(() =>
-    withTiming(height.value * Number(isExpanded.value), {
-      duration: 500,
-    })
-  );
-  const bodyStyle = useAnimatedStyle(() => ({
-    height: derivedHeight.value,
-  }));
-
-  return (
-    <Animated.View style={[bodyStyle, { overflow: "hidden" }]}>
-      <View
-        onLayout={(e) => {
-          height.value = e.nativeEvent.layout.height;
-        }}
-        style={{ position: "absolute" }}
-      >
-        {children}
-      </View>
-    </Animated.View>
-  );
-}
-
-const headerStyle: BoxProps = {
-  flexDirection: "row",
-  justifyContent: "space-between",
-  padding: "s16",
-  // borderWidth: 2,
-  // borderColor: "gray1",
-  // borderRadius: "default",
-  alignItems: "center",
-};
--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -1,44 +1,163 @@
+import { Pressable, StyleSheet, View } from "react-native";
 import Animated, {
   interpolate,
+  interpolateColor,
   SharedValue,
   useAnimatedStyle,
+  useDerivedValue,
+  useSharedValue,
+  withTiming,
 } from "react-native-reanimated";
 
-import { useAppTheme } from "../theme/useAppTheme";
+import theme from "../theme/theme";
+import { Icon } from "./Icon";
 import { Text } from "./Text";
 
 type AccordionProps = {
   title: string;
   description: string;
-  progress: SharedValue<number>;
+  duration?: number;
 };
 
-export function Accordion({ description, progress }: AccordionProps) {
-  const { colors, spacing, borderRadii } = useAppTheme();
+export function Accordion({
+  title,
+  description,
+  duration = 500,
+}: AccordionProps) {
+  const isExpanded = useSharedValue(false);
 
-  const animatedStyle = useAnimatedStyle(() => ({
-    paddingHorizontal: spacing.s16,
-    paddingBottom: spacing.s16,
-    backgroundColor: colors.gray1,
-    borderBottomEndRadius: borderRadii.default,
-    borderBottomStartRadius: borderRadii.default,
-    borderTopLeftRadius: interpolate(
+  const onPress = () => {
+    isExpanded.value = !isExpanded.value;
+    progress.value = withTiming(isExpanded.value ? 0 : 1, { duration });
+  };
+
+  const progress = useSharedValue(0);
+
+  return (
+    <Pressable onPress={onPress}>
+      <AccordionHeader
+        title={title}
+        progress={progress}
+        isExpanded={isExpanded}
+        duration={duration}
+      />
+      <AccordionBody
+        description={description}
+        progress={progress}
+        isExpanded={isExpanded}
+        duration={duration}
+      />
+    </Pressable>
+  );
+}
+
+function AccordionHeader({
+  title,
+  progress,
+  isExpanded,
+  duration,
+}: {
+  title: string;
+  progress: SharedValue<number>;
+  isExpanded: SharedValue<boolean>;
+  duration: number;
+}) {
+  const iconStyle = useAnimatedStyle(() => ({
+    transform: [
+      {
+        rotate: isExpanded.value
+          ? withTiming("-180deg", { duration })
+          : withTiming("0deg", { duration }),
+      },
+    ],
+  }));
+
+  const animatedHeaderStyled = useAnimatedStyle(() => ({
+    backgroundColor: interpolateColor(
       progress.value,
       [0, 1],
-      [borderRadii.default, 0]
+      [theme.colors.transparent, theme.colors.gray1]
     ),
-    borderTopRightRadius: interpolate(
+    borderBottomWidth: interpolate(progress.value, [0, 1], [2, 0]),
+    borderColor: theme.colors.gray1,
+    borderBottomLeftRadius: interpolate(
       progress.value,
       [0, 1],
-      [borderRadii.default, 0]
+      [theme.borderRadii.default, 0]
     ),
-    borderWidth: 2,
-    borderColor: colors.gray1,
+    borderBottomRightRadius: interpolate(
+      progress.value,
+      [0, 1],
+      [theme.borderRadii.default, 0]
+    ),
   }));
 
   return (
-    <Animated.View style={animatedStyle}>
-      <Text variant="text14">{description}</Text>
+    <Animated.View style={[animatedHeaderStyled, styles.header]}>
+      <Text variant="title16">{title}</Text>
+
+      <Animated.View style={iconStyle}>
+        <Icon name="Chevron-down" color={"gray2"} />
+      </Animated.View>
     </Animated.View>
   );
 }
+
+function AccordionBody({
+  description,
+  progress,
+  isExpanded,
+  duration,
+}: {
+  description: string;
+  progress: SharedValue<number>;
+  isExpanded: SharedValue<boolean>;
+  duration: number;
+}) {
+  const height = useSharedValue(0);
+
+  const derivedHeight = useDerivedValue(() =>
+    withTiming(height.value * Number(isExpanded.value), {
+      duration,
+    })
+  );
+
+  const animatedBodyStyle = useAnimatedStyle(() => ({
+    height: derivedHeight.value,
+    opacity: interpolate(progress.value, [0, 1], [0, 1]),
+  }));
+
+  return (
+    <Animated.View style={[animatedBodyStyle, { overflow: "hidden" }]}>
+      <View
+        onLayout={(e) => {
+          height.value = e.nativeEvent.layout.height;
+        }}
+        style={styles.content} // workaround 1
+      >
+        <Text variant="text14">{description}</Text>
+      </View>
+    </Animated.View>
+  );
+}
+
+const styles = StyleSheet.create({
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: theme.spacing.s16,
+    alignItems: "center",
+    borderWidth: 2,
+    borderColor: theme.colors.gray1,
+    borderTopLeftRadius: theme.borderRadii.default,
+    borderTopRightRadius: theme.borderRadii.default,
+  },
+  content: {
+    position: "absolute",
+    backgroundColor: theme.colors.gray1,
+    paddingHorizontal: theme.spacing.s16,
+    paddingBottom: theme.spacing.s16,
+    borderBottomEndRadius: theme.borderRadii.default,
+    borderBottomStartRadius: theme.borderRadii.default,
+  },
+});
