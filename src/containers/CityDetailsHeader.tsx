a1a823e72e0971a93bc788863ccc032e4abd6d80
Lucas Garcez <lucasgarcezdev@gmail.com>
2025-05-24 22:54:34 +1000

    3 - CityDetails

--- src/containers/CityDetailsHeader.tsx
+++ src/containers/CityDetailsHeader.tsx
@@ -1,10 +1,36 @@
+import { router } from "expo-router";
+import { ImageBackground } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { Box } from "../components/Box";
-import { Text } from "../components/Text";
+import { Icon } from "../components/Icon";
+import { IconButton } from "../components/IconButton";
+import { City } from "../types";
 
-export function CityDetailsHeader() {
+type CityDetailsHeaderProps = Pick<City, "id" | "coverImage" | "categories">;
+
+export function CityDetailsHeader({
+  coverImage,
+  categories,
+}: CityDetailsHeaderProps) {
+  const { top } = useSafeAreaInsets();
   return (
-    <Box padding="padding">
-      <Text>CityDetailsHeader</Text>
+    <Box>
+      <ImageBackground
+        source={coverImage}
+        style={{ width: "100%", height: 250 }}
+        imageStyle={{ borderBottomRightRadius: 40 }}
+      >
+        <Box
+          flexDirection="row"
+          justifyContent="space-between"
+          alignItems="center"
+          padding="padding"
+          style={{ paddingTop: top }}
+        >
+          <IconButton iconName="Chevron-left" onPress={router.back} />
+          <Icon name="Favorite-outline" size={30} color="pureWhite" />
+        </Box>
+      </ImageBackground>
     </Box>
   );
 }
