c6ea4f54422974c632b7bc41e39194985e68a626
Lucas Garcez <lucasgarcezdev@gmail.com>
2025-05-25 19:23:40 +1000

    8 - Accordion base structure

--- src/containers/CityDetailsTouristAttractions.tsx
+++ src/containers/CityDetailsTouristAttractions.tsx
@@ -1,10 +1,24 @@
+import { Accordion } from "../components/Accordion";
 import { Box } from "../components/Box";
 import { Text } from "../components/Text";
+import { City } from "../types";
 
-export function CityDetailsTouristAttractions() {
+type Props = Pick<City, "touristAttractions">;
+export function CityDetailsTouristAttractions({ touristAttractions }: Props) {
   return (
     <Box padding="padding">
-      <Text>CityDetailsTouristAttractions</Text>
+      <Text variant="title22" mb="s8">
+        Pontos turísticos
+      </Text>
+      <Box gap="s8">
+        {touristAttractions.map((attraction) => (
+          <Accordion
+            key={attraction.id}
+            title={attraction.name}
+            description={attraction.description}
+          />
+        ))}
+      </Box>
     </Box>
   );
 }
