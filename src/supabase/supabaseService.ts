9f2fe56c933be2cf26fa2fd6cfedf1f405271bf3
Lucas Garcez <lucasgarcezdev@gmail.com>
2025-06-07 10:52:53 +1000

    6 - useCities using supabase

--- src/supabase/supabaseService.ts
+++ src/supabase/supabaseService.ts
@@ -1,8 +1,25 @@
+import { CityPreview } from "../types";
 import { supabase } from "./supabase";
 
-async function findAll() {
-  const cities = await supabase.from("cities").select("id,name,description");
-  console.log({ cities });
+const storageURL = process.env.EXPO_PUBLIC_SUPABASE_STORAGE_URL;
+
+async function findAll(): Promise<CityPreview[]> {
+  try {
+    const { data } = await supabase.from("cities").select("*");
+
+    if (!data) {
+      throw new Error("data is not available");
+    }
+
+    return data?.map((row) => ({
+      id: row.id,
+      country: row.country,
+      name: row.name,
+      coverImage: `${storageURL}/${row.cover_image}`,
+    }));
+  } catch (error) {
+    throw error;
+  }
 }
 
 export const supabaseService = { findAll };
