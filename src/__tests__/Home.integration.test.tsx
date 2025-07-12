09cf02eb1458cf1adb85693b25bcb23923a73a8a
Lucas Garcez <lucasgarcezdev@gmail.com>
2025-07-12 19:56:16 +1000

    15 - testing error, loading and empty states.

--- src/__tests__/Home.integration.test.tsx
+++ src/__tests__/Home.integration.test.tsx
@@ -25,4 +25,40 @@ describe("integration: Home", () => {
     expect(screen.getByText("Barcelona")).toBeOnTheScreen();
     expect(screen.getByText("Espanha")).toBeOnTheScreen();
   });
+
+  it("should display an error message when city list does not load", async () => {
+    renderApp({
+      isAuthenticated: true,
+      repositories: {
+        city: {
+          findAll: async () => {
+            return Promise.reject(new Error("server is down!"));
+          },
+        },
+      },
+    });
+
+    expect(
+      await screen.findByText(/erro ao carregar cidades/i)
+    ).toBeOnTheScreen();
+    expect(await screen.findByText(/server is down!/i)).toBeOnTheScreen();
+  });
+
+  it("should display an empty message when city list is empty", async () => {
+    renderApp({
+      isAuthenticated: true,
+      repositories: {
+        city: {
+          findAll: async () => {
+            return [];
+          },
+        },
+      },
+    });
+
+    expect(await screen.findByText(/carregando cidades/i)).toBeOnTheScreen();
+    expect(
+      await screen.findByText(/não há cidades no momento/i)
+    ).toBeOnTheScreen();
+  });
 });
