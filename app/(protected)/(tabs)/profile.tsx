ea98c8c682cf7b2390a2961273a9c24f04a3a8eb
Lucas Garcez <lucasgarcezdev@gmail.com>
2025-07-29 21:16:55 +1000

    17 - useFindAllFavorites

--- app/(protected)/(tabs)/profile.tsx
+++ app/(protected)/(tabs)/profile.tsx
@@ -1,6 +1,8 @@
 import { useAuthGetUser } from "@/src/domain/auth/operations/useAuthGetUser";
 import { useAuthSignOut } from "@/src/domain/auth/operations/useAuthSignOut";
+import { useFindAllFavorites } from "@/src/domain/city/operations/useFindAllFavorites";
 import { Box } from "@/src/ui/components/Box";
+import { CityCard } from "@/src/ui/components/CityCard";
 import { Icon } from "@/src/ui/components/Icon";
 import { Screen } from "@/src/ui/components/Screen";
 import { Text } from "@/src/ui/components/Text";
@@ -12,11 +14,17 @@ import { SafeAreaView } from "react-native-safe-area-context";
 export default function ProfileScreen() {
   const { mutate: signOut } = useAuthSignOut();
   const { data: authUser } = useAuthGetUser();
+  const { data: favoriteList } = useFindAllFavorites();
 
   return (
     <Screen>
       <SafeAreaView>
         {authUser && <ProfileHeader authUser={authUser} />}
+
+        {favoriteList?.map((cityPreview) => (
+          <CityCard key={cityPreview.id} cityPreview={cityPreview} />
+        ))}
+
         <Pressable onPress={signOut}>
           <Box
             mt="s24"
