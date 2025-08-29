df4249b29510839ee909cba3b45e243946db1c0b
Lucas Garcez <lucasgarcezdev@gmail.com>
2025-08-29 22:26:00 +1000

    7 - config app to build

---
+++ app.config.ts
@@ -0,0 +1,52 @@
+module.exports = {
+  name: "Nomad",
+  slug: "digital-nomad-app",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/images/icon.png",
+  scheme: "digitalnomadapp",
+  userInterfaceStyle: "automatic",
+  newArchEnabled: true,
+  ios: {
+    supportsTablet: true,
+    bundleIdentifier: "com.coffstack.digitalnomadapp",
+    infoPlist: {
+      ITSAppUsesNonExemptEncryption: false,
+    },
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: "./assets/images/adaptive-icon.png",
+      backgroundColor: "#1B1B1B",
+    },
+    edgeToEdgeEnabled: true,
+    package: "com.coffstack.digitalnomadapp",
+  },
+  web: {
+    bundler: "metro",
+    output: "static",
+    favicon: "./assets/images/favicon.png",
+  },
+  plugins: [
+    "expo-router",
+    [
+      "expo-splash-screen",
+      {
+        image: "./assets/images/splash-icon.png",
+        imageWidth: 200,
+        resizeMode: "contain",
+        backgroundColor: "#1B1B1B",
+      },
+    ],
+    "expo-web-browser",
+  ],
+  experiments: {
+    typedRoutes: true,
+  },
+  extra: {
+    router: {},
+    eas: {
+      projectId: "03f30d93-abb0-463f-901f-09d172493029",
+    },
+  },
+};
