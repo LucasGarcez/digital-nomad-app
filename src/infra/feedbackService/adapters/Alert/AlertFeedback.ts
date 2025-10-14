4c068c93b0b06b6458666b1e3196946e5c7a43ab
Lucas Garcez <lucasgarcezdev@gmail.com>
2025-10-14 19:14:21 +1100

    force build

--- src/infra/feedbackService/adapters/Alert/AlertFeedback.ts
+++ src/infra/feedbackService/adapters/Alert/AlertFeedback.ts
@@ -1,6 +1,7 @@
 import { Alert } from "react-native";
 import { IFeedbackService } from "../../IFeedbackService";
 
+// force trigger a build
 export const AlertFeedback: IFeedbackService = {
   send: (feedback) => {
     Alert.alert(feedback.message, feedback.description);
